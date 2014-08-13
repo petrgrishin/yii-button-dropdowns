c27a1942d63c465556d0f6c2685eaa9f654693a9
Petr Grishin <petr.grishin@grishini.ru>
2014-08-14 00:49:08 +0400

    Подготовка каркаса для класса виджета кнопки с множеством действий

---
+++ src/ButtonDropdownsWidget.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+namespace PetrGrishin\ButtonDropdowns;
+
+
+use PetrGrishin\ButtonDropdowns\Exception\ButtonDropdownsWidgetException;
+use PetrGrishin\Widget\BaseWidget;
+
+class ButtonDropdownsWidget extends BaseWidget {
+    const TYPE_DEFAULT = 'default';
+    const TYPE_PRIMARY = 'primary';
+    const TYPE_SUCCESS = 'success';
+    const TYPE_INFO = 'info';
+    const TYPE_WARNING = 'warning';
+    const TYPE_DANGER = 'danger';
+    const TYPE_LINK = 'link';
+
+    /** @var string */
+    private $title;
+    /** @var string */
+    private $type;
+
+    private static $types = array(
+        self::TYPE_DEFAULT,
+        self::TYPE_PRIMARY,
+        self::TYPE_SUCCESS,
+        self::TYPE_INFO,
+        self::TYPE_WARNING,
+        self::TYPE_DANGER,
+        self::TYPE_LINK,
+    );
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function setType($type) {
+        if (false === array_search($type, self::$types)) {
+            throw new ButtonDropdownsWidgetException(sprintf('Type `%s` is illegal', $type));
+        }
+        $this->type = $type;
+        return $this;
+    }
+}
+ 
---
+++ src/Exception/ButtonDropdownsWidgetException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+namespace PetrGrishin\ButtonDropdowns\Exception;
+
+
+class ButtonDropdownsWidgetException extends \Exception {
+
+}
+ 
